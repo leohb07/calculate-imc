5680871e64a076c5a7f39e76cc96ad20496e5afe
leohb07 <leohb1501@gmail.com>
2022-05-05 23:51:23 -0300

    Projeto criado e tambem feito a sua UI

---
+++ CalcIMC/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CalcIMC
+//
+//  Created by Leonardo Henrique Barrocal on 04/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  
+  @IBOutlet weak var weightTextField: UITextField!
+  @IBOutlet weak var heightTextField: UITextField!
+  @IBOutlet weak var typeIMCLabel: UILabel!
+  @IBOutlet weak var imageImageView: UIImageView!
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  @IBAction func tappedCalculateButton(_ sender: Any) {
+    
+    // pegar valores do usuario
+    
+    // calcular
+    
+    // mostrar resultado conforme a operacao mostrar
+    
+    
+  }
+  
+
+}
+

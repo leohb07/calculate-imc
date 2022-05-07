8fde784b28df0b7e6764f233604df4b78a1ca5dd
leohb07 <leohb1501@gmail.com>
2022-05-06 23:34:35 -0300

    Projeto finalizado, desinvolvimento do calculo feito e ajustes do layout UI

--- CalcIMC/ViewController.swift
+++ CalcIMC/ViewController.swift
@@ -14,25 +14,55 @@ class ViewController: UIViewController {
   @IBOutlet weak var heightTextField: UITextField!
   @IBOutlet weak var typeIMCLabel: UILabel!
   @IBOutlet weak var imageImageView: UIImageView!
+  @IBOutlet weak var resultView: UIView!
   
-
+  var imcResult: Double = 0
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
   
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
   
   @IBAction func tappedCalculateButton(_ sender: Any) {
     
-    // pegar valores do usuario
-    
-    // calcular
-    
-    // mostrar resultado conforme a operacao mostrar
-    
-    
+    if let weight = Double(weightTextField.text!), let height = Double(heightTextField.text!){
+      imcResult = weight / pow(height, 2)
+      showResult()
+      
+    }
   }
   
-
+  func showResult(){
+    var result: String = ""
+    var imageView: String = ""
+    
+    switch imcResult {
+      case 0..<16:
+        result = "Magreza"
+        imageView = "abaixo"
+      case 16..<18.5:
+        result = "Abaixo do peso"
+        imageView = "abaixo"
+      case 18.5..<25:
+        result = "Peso ideal"
+        imageView = "ideal"
+      case 25..<30:
+        result = "Sobrepeso"
+        imageView = "sobre"
+      default:
+        result = "Obesidade"
+        imageView = "obesidade"
+    }
+    typeIMCLabel.text = "\(imcResult.rounded()): \(result)"
+    imageImageView.image = UIImage(named: imageView)
+    resultView.isHidden = false
+    view.endEditing(true) // forca o app a nao ter mais foco ao clica fora de campos que podem ser focados
+  }
+  
 }
+
 
